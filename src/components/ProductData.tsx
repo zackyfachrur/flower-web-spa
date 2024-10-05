14ba773cd2eb6187249c6fff0b882fdaa73dd606
zackyfachrur <mochzackyaja@gmail.com>
2024-10-05 19:02:21 +0700

    Product Page Component

---
+++ src/components/ProductData.tsx
@@ -0,0 +1,61 @@
+import Flower1 from "../assets/product/flower-1.jpeg";
+import Flower2 from "../assets/product/flower-2.jpeg";
+import Flower3 from "../assets/product/flower-3.jpeg";
+import Flower4 from "../assets/product/flower-4.jpeg";
+import Flower5 from "../assets/product/flower-5.jpeg";
+import Flower6 from "../assets/product/flower-6.jpeg";
+import Flower7 from "../assets/product/flower-7.jpeg";
+import Flower8 from "../assets/product/flower-8.jpeg";
+import Flower9 from "../assets/product/flower-9.jpeg";
+import Flower10 from "../assets/product/flower-10.jpeg";
+
+const ImageGallery = [
+  {
+    id: 1,
+    name: "White & Red Rosses",
+    link: { Flower1 },
+  },
+  { id: 2, name: "Pink Golden Rosses", link: { Flower2 } },
+  {
+    id: 3,
+    name: "Pink Golden Rosses",
+    link: { Flower3 },
+  },
+  {
+    id: 4,
+    name: "Pink Golden Rosses",
+    link: { Flower4 },
+  },
+  {
+    id: 5,
+    name: "Pink Golden Rosses",
+    link: { Flower5 },
+  },
+  {
+    id: 6,
+    name: "Pink Golden Rosses",
+    link: { Flower6 },
+  },
+  {
+    id: 7,
+    name: "Pink Golden Rosses",
+    link: { Flower7 },
+  },
+  {
+    id: 8,
+    name: "Pink Golden Rosses",
+    link: { Flower8 },
+  },
+  {
+    id: 9,
+    name: "Pink Golden Rosses",
+    link: { Flower9 },
+  },
+  {
+    id: 10,
+    name: "Pink Golden Rosses",
+    link: { Flower10 },
+  },
+];
+
+export default ImageGallery;
