74c29adc44656904a806d052e8d826004234c77b
zackyfachrur <mochzackyaja@gmail.com>
2024-10-06 11:14:01 +0700

    New Product Page UI

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -20,27 +20,42 @@ function NavLink({ Link, Content, className }: NavLinkProps): React.ReactNode {
 const NavigationMenu = (): React.ReactNode => {
   return (
     <nav className="h-20 flex justify-around items-center">
-      <img src={NavLogo} alt="Nav Icon" className="w-52" />
+      <div className="flex justify-center items-center">
+        {/* Logo */}
+        <img src={NavLogo} alt="Nav Icon" className="w-52" />
+        {/* Search */}
+        <div className="flex gap-2 bg-transparent border-2 border-rose-500 px-5 py-1 text-md rounded-full">
+          <input
+            type="text"
+            className="bg-transparent outline-none focus:outline-none w-72 text-md font-semibold"
+            placeholder="Cari Produk..."
+          />
+          <button className="bg-rose-500 px-5 rounded-full">
+            <i className="ri-search-line text-white "></i>
+          </button>
+        </div>
+      </div>
+      {/* Nav Ul */}
       <ul className="flex justify-center items-center">
         <NavLink
           Link="#"
           Content="Product"
-          className="font-semibold text-xl text-white bg-rose-500 px-5 py-2 rounded-l-full hover:bg-white border-2 border-rose-500 hover:text-rose-500 hover:border-rose-500"
+          className="font-semibold text-md text-white bg-rose-500 px-5 py-2 hover:bg-white border-2 border-rose-500 hover:text-rose-500 hover:border-rose-500 rounded-l-full"
         />
         <NavLink
           Link="#"
           Content="About"
-          className="font-semibold text-xl text-white bg-rose-500 px-5 py-2 hover:bg-white border-2 border-rose-500 hover:text-rose-500 hover:border-rose-500"
+          className="font-semibold text-md text-white bg-rose-500 px-5 py-2 hover:bg-white border-2 border-rose-500 hover:text-rose-500 hover:border-rose-500"
         />
         <NavLink
           Link="#"
           Content="Contact"
-          className="font-semibold text-xl text-white bg-rose-500 px-5 py-2 hover:bg-white border-2 border-rose-500 hover:text-rose-500 hover:border-rose-500"
+          className="font-semibold text-md text-white bg-rose-500 px-5 py-2 hover:bg-white border-2 border-rose-500 hover:text-rose-500 hover:border-rose-500"
         />
         <NavLink
           Link="#"
           Content="FAQ"
-          className="font-semibold text-xl text-white bg-rose-500 px-5 py-2 rounded-r-full hover:bg-white border-2 border-rose-500 hover:text-rose-500 hover:border-rose-500"
+          className="font-semibold text-md text-white bg-rose-500 px-5 py-2 rounded-r-full hover:bg-white border-2 border-rose-500 hover:text-rose-500 hover:border-rose-500"
         />
       </ul>
     </nav>
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,101 +1,22 @@
 import NavigationMenu from "../components/Navigation";
-import Flower1 from "../assets/product/flower-1.jpeg";
-
-interface FlowerCardProps {
-  Link: string;
-}
-
-const FlowerCard = ({ Link }: FlowerCardProps): React.ReactNode => {
-  return (
-    <div className="w-1/2 max-w-sm  border-rose-200 border-2 rounded-lg shadow bg-rose-500">
-      <a href="#">
-        <img className="pb-5 w-full" src={Link} alt="product image" />
-      </a>
-      <div className="px-5 pb-5">
-        <a href="#">
-          <h5 className="text-lg font-semibold tracking-tight text-white">
-            Red & White Rosses
-          </h5>
-        </a>
-        <div className="flex items-center mt-2.5 mb-5">
-          <div className="flex items-center space-x-1 rtl:space-x-reverse">
-            <svg
-              className="w-4 h-4 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-yellow-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-gray-200"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-          </div>
-          <span className=" text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded bg-blue-200  ms-3">
-            5.0
-          </span>
-        </div>
-        <div className="flex items-center justify-between">
-          <span className="text-md font-bold text-white">Rp. 250,000,00</span>
-          <a
-            href="#"
-            className="text-white focus:ring-4 focus:outline-none focus:ring-rose-500 font-medium rounded-lg text-sm px-3 py-2 text-center bg-rose-700 hover:bg-rose-800 "
-          >
-            Order Product
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-};
+import FlowerCardContent from "../components/FlowerCard";
+import ImageGallery from "../components/ProductData";
 
 const Product = (): React.ReactNode => {
   return (
     <>
       <NavigationMenu />
       <section className="w-full justify-center items-center">
-        <div className="flex justify-start gap-5 flex-wrap">
-          <FlowerCard Link={Flower1} />
-          <FlowerCard Link={Flower1} />
-          <FlowerCard Link={Flower1} />
-          <FlowerCard Link={Flower1} />
-          <FlowerCard Link={Flower1} />
-          <FlowerCard Link={Flower1} />
-          <FlowerCard Link={Flower1} />
+        <div className="flex justify-center gap-5 flex-wrap">
+          {ImageGallery.map((flower) => (
+            <FlowerCardContent
+              key={flower.id}
+              Link={flower.link}
+              Name={flower.name}
+              Price={flower.price}
+              CTA="#"
+            />
+          ))}
         </div>
       </section>
     </>
